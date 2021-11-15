c46b536661ecad7fd2c4a551b5f214816f70807d
syuancao <73942181+syuancao@users.noreply.github.com>
2021-11-15 23:46:18 +0800

    组件生命周期(2)

--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 export interface TopMenu {
   title: string;
@@ -16,7 +16,7 @@ export interface TopMenu {
  * 但建议实现接口，好处一个是不会由于误删除某个钩子函数
  * 另一个是对组件涉及到哪些生命周期一目了然
  */
-export class ScrollableTabComponent implements OnInit, OnChanges {
+export class ScrollableTabComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   selectedIndex = -1
   @Input() menus: TopMenu[] = []
@@ -31,6 +31,10 @@ export class ScrollableTabComponent implements OnInit, OnChanges {
   constructor() {
     console.log('组件构造调用')
   }
+  /**
+   * 在组件的 `@Input` 属性发生变化的时候调用
+   * @param changes 索引对象，key 是属性的名字，value 是 SimpleChanges
+   */
   ngOnChanges(changes: SimpleChanges): void {
     console.log('组件输入属性改变', changes)
   }
@@ -40,9 +44,39 @@ export class ScrollableTabComponent implements OnInit, OnChanges {
   ngOnInit(): void {
     console.log('组件初始化')
   }
+  /**
+   * 组件内容初始化
+   */
+  ngAfterContentInit(): void {
+    console.log('组件内容初始化')
+  }
+  /**
+   * 组件内容脏值检测
+   */
+  ngAfterContentChecked(): void {
+    console.log('组件内容脏值检测')
+  }
+  /**
+   * 组件的视图初始化
+   */
+  ngAfterViewInit(): void {
+    console.log('组件视图初始化')
+  }
+  /**
+   * 组件视图脏值检测
+   */
+  ngAfterViewChecked(): void {
+    console.log('组件视图脏值检测')
+  }
 
   ngDoCheck(): void {
     console.log('组件脏值检测')
+  }
+  /**
+   * 需要做一些清理工作
+   */
+  ngOnDestroy(): void {
+    console.log('组件销毁')
   }
 
   handleSelection(index: number) {
