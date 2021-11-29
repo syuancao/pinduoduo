bfdb1415dfcdff8f7e9bb820e257b1a828bb92c6
syuancao <73942181+syuancao@users.noreply.github.com>
2021-11-29 12:38:18 +0800

    注解是一个返回函数的函数

--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Confirmable, Emoji } from '../../decorators';
 
 @Component({
   selector: 'app-horizontal-grid',
@@ -8,6 +9,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class HorizontalGridComponent implements OnInit {
   private _username = ''
   @Output() usernameChange = new EventEmitter()
+  @Emoji() result = 'Hello' 
   constructor() { }
 
   ngOnInit(): void {
@@ -21,5 +23,10 @@ export class HorizontalGridComponent implements OnInit {
   public set username(value : string) {
     this._username = value;
     this.usernameChange.emit(value)
+  }
+
+  @Confirmable('您确认要执行吗？')
+  handleClick() {
+    console.log('点击已执行')
   }
 }
